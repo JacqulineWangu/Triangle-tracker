781d3c4ad8be19792253e3ca5b4a33add0d9a091
jacquline <jacqulinewangu@gmail.com>
2019-03-25 08:32:45 +0300

    add PROPERTIES OF TRIANGLES

---
+++ JS/scripts.js
@@ -0,0 +1,17 @@
+function calc() {
+  var lengthA = [parseFloat(document.querySelector("#sideA").value)]
+  var lengthB = [parseFloat(document.querySelector("#sideB").value)]
+  var lengthC = [parseFloat(document.querySelector("#sideC").value)]
+
+
+var ab = lengthA.concat(lengthB);
+
+var abc = ab.concat(lengthC);
+
+var sorted = abc.sort(function(d, e)
+return d - e;
+})
+
+var a = sorted[0]
+var b = sorted[1]
+var c = sorted[2]
