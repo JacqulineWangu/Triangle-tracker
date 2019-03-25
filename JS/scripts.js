271c48a0417efadc64a9f2239a932f3bc8521526
jacquline <jacqulinewangu@gmail.com>
2019-03-25 12:24:08 +0300

    made changes to my js script

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,39 +1,40 @@
 function calc() {
-  var lengthA = [parseFloat(document.querySelector("#sideA").value)]
-  var lengthB = [parseFloat(document.querySelector("#sideB").value)]
-  var lengthC = [parseFloat(document.querySelector("#sideC").value)]
+  var a = parseFloat(document.querySelector("#sideA").value);
+  var b = parseFloat(document.querySelector("#sideB").value);
+  var c = parseFloat(document.querySelector("#sideC").value);
 
 
-var ab = lengthA.concat(lengthB);
+// var ab = lengthA.concat(lengthB);
+//
+// var abc = ab.concat(lengthC);
 
-var abc = ab.concat(lengthC);
-
-var sorted = abc.sort(function(d, e)
-return d - e;
+// var sorted = abc.sort(function(d, e)
+// return d - e;
 })
 
-var a = sorted[0]
-var b = sorted[1]
-var c = sorted[2]
+// var a = sorted[0]
+// var b = sorted[1]
+// var c = sorted[2]
 
-var lengthab = a + b
+var ab = a + b
 
-var respond;
+var respond = document.querySelector("#Answer");
 
-if(lengthab < c){
-  respond = "ERROR! Not a triangle"
+if(ab < c){
+  respond.innerHTML = "ERROR! Not a triangle";
 }
 else if(a === b && b === c && a > 0 && b > 0 && c > 0){
   console.log('EQUILATERAL TRIANGLE')
-respond = 'EQUILATERAL TRIANGLE'
-}else if(a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0){
+respond.innerHTML= 'EQUILATERAL TRIANGLE';
+}
+else if(a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0){
   console.log('ISOSCELES TRIANGLE')
-  respond = 'ISOSCELES TRIANGLE'
-}else if(a !== b && b !== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(C) === true) && a > 0 && b > 0 && c > 0){
+  respond.innerHTML = 'ISOSCELES TRIANGLE';
+}
+else if(a !== b && b !== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(C) === true) && a > 0 && b > 0 && c > 0){
   console.log('SCALENE TRIANGLE')
-  respond = 'SCALENE TRIANGLE'
-}else { respond = "Invalid measurements. Kindly re-evaluate your figures."};
-
-document.querySelector("#Answer").innerHTML = respond
-
+  respond.innerHTML = 'SCALENE TRIANGLE';
+}
+else {
+respond = "Invalid measurements. Kindly re-evaluate your figures."
 }
